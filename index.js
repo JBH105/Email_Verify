e6776ecbe8b02c26b89eba74fe07536d44f1f1f4
jbh105 <jaydipbhayani104@gmail.com>
2022-08-20 17:20:05 +0530

    made the image function

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const user = require("./routes/api/User");
 const event = require("./routes/api/Event");
 const login = require("./routes/api/Login.js");
 const bill = require("./routes/api/Bill");
+const image = require("./routes/api/image")
 const PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -20,11 +21,15 @@ app.get("/", async (req, res) => {
   res.sendFile(path.join(__dirname + "/views/index.html"));
 });
 
+// IMAGES
+app.use("/public", express.static("Images"))
+
+// API ROUTES
 app.use("/api", user);
 app.use("/api/event", event)
 app.use("/api/login", login)
 app.use("/api/bill", bill)
-
+app.use("/api/img", image)
 
 
 app.listen(PORT, () => {
